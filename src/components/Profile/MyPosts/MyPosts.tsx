0054b91a04c297e18115717dd5d00d50486d31cc
Barash777 <barash.vertihvost@gmail.com>
2022-05-27 11:44:37 +0300

    lesson 16

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -10,9 +10,9 @@ function MyPosts() {
                 New posts
             </div>
             <div className={css.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message={'Hello!'} likeCount={12}/>
+                <Post message={'Wow'} likeCount={5}/>
+                <Post message={'It\'s my first message. Stop it!'} likeCount={37}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import css from './Post.module.css';
 
-function Post() {
+type PropsType = {
+    message: string
+    likeCount: number
+}
+
+function Post(props: PropsType) {
     return (
         <div className={css.item}>
             <img src="https://upload.wikimedia.org/wikipedia/ru/thumb/c/ce/Aang.png/280px-Aang.png" alt=""/>
-            Post1
+            {props.message}
+            <div>{props.likeCount}<span> like(s)</span></div>
         </div>
     );
 }
