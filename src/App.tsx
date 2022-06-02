178218e7070dae7325796cfe8f1782a6745f3924
Barash777 <barash.vertihvost@gmail.com>
2022-06-02 23:29:34 +0300

    lesson 17

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import './css/App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 function App() {
     return (
         <div className="app-wrapper">
-
             <Header/>
             <Sidebar/>
-            <Profile/>
+
+            <div className="app-wrapper-content">
+                <Dialogs/>
+                {/*<Profile/>*/}
+            </div>
 
         </div>
     );
