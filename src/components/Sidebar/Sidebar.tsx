a5c6b8fdae84b4240dad1fe4f298a5b041a06b2f
Barash777 <barash.vertihvost@gmail.com>
2022-06-03 17:30:38 +0300

    lesson 20

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import css from './Sidebar.module.css';
+import {NavLink} from 'react-router-dom';
+
 
 function Sidebar() {
+
     return (
         <nav className={css.sidebar}>
-            <div className={`${css.item} ${css.active}`}>
-                <a href="/profile">Profile</a>
+            <div className={css.item}>
+                <NavLink to="/profile" activeClassName={css.active}>Profile</NavLink>
             </div>
             <div className={css.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs" activeClassName={css.active}>Messages</NavLink>
             </div>
             <div className={css.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news" activeClassName={css.active}>News</NavLink>
             </div>
             <div className={css.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music" activeClassName={css.active}>Music</NavLink>
             </div>
             <div className={css.item}>
-                <a href="/settings">Settings</a>
+                <NavLink to="/settings" activeClassName={css.active}>Settings</NavLink>
             </div>
         </nav>
     );
 }
 
 export default Sidebar;
+
+/*
+у кого в конце 21го не работает activeClassName - необходимо заменить на
+className = { navData => navData.isActive ? s.active : s.item }
++ пишем правило в css для .active {color: gold;} + там же переименовываем ".item a" просто в ".item"
+<div className={s.item}> тоже можно заменить на просто <div>, от класснэйма тут толку не будет
+ */
