5ca7e6a5abc66140fb1dc13ada5cc1a487d21c40
Barash777 <barash.vertihvost@gmail.com>
2022-06-03 17:52:36 +0300

    lesson 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,35 @@ import css from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={css.dialogsPage}>
+            <div className={css.dialogs}>
+                <div className={css.dialogItem + ' ' + css.active}>
+                    Olga
+                </div>
+                <div className={css.dialogItem}>
+                    Ilya
+                </div>
+                <div className={css.dialogItem}>
+                    Anton
+                </div>
+                <div className={css.dialogItem}>
+                    Tanya
+                </div>
+                <div className={css.dialogItem}>
+                    Victor
+                </div>
+            </div>
+            <div className={css.messages}>
+                <div className={css.messageItem}>
+                    Hi
+                </div>
+                <div className={css.messageItem}>
+                    Hello
+                </div>
+                <div className={css.messageItem}>
+                    What's up?
+                </div>
+            </div>
         </div>
     );
 };
